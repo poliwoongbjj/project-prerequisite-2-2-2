90fa5057fb9565a390d41fbaf0ee5182a0fd406f
Woong Kim <154739147+poliwoongbjj@users.noreply.github.com>
2025-07-17 18:15:29 +0900

    Create the Cars Controller

---
+++ src/main/java/web/controller/CarsController.java
@@ -0,0 +1,31 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+import web.service.CarService;
+
+@Controller
+@RequestMapping("/cars")
+public class CarsController {
+
+    private final CarService carService;
+
+    @Autowired
+    public CarsController(CarService carService) {
+        this.carService = carService;
+    }
+
+    @GetMapping
+    public String getCars(@RequestParam(value = "count", required = false) Integer count, Model model) {
+        if (count == null) {
+            model.addAttribute("cars", carService.getAllCars());
+        } else {
+            model.addAttribute("cars", carService.getCars(count));
+        }
+        return "cars";
+    }
+} 
